46b430b6f708b655cffba7b38c9d9cfeaa51e07c
FacerAin <ywsong.dev@kakao.com>
2023-02-19 13:37:08 +0900

    feat: update gastby-config for title and author

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,10 +10,10 @@
 module.exports = {
   pathPrefix: `/dcom-landing-page`,
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `D.COM`,
+    description: `dcom landing page`,
+    author: `@facerain`,
+    siteUrl: `https://dcom-khu.github.io/dcom-landing-page/`,
   },
   plugins: [
     {
